02e74c4e23b9aaa023f9dabc4913e6bcfed598f0
Tucker Fredrickson <tuck.fredrickson@gmail.com>
2016-11-23 16:09:35 -0800

    basic functionality

---
+++ js/quiz.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+$('#submit').click(function(event){
+	event.preventDefault();
+	var searchTerm = $('#query').val();
+	getRequest(searchTerm);
+});
+
+
+function getRequest(searchTerm){
+	var params = {
+		q: searchTerm,
+		part: 'snippet',
+		type: 'video',
+		key: 'AIzaSyBoNFfAExXIcb93jlzRlUYqG-d3eCJTZM4',
+		maxResults: '10'
+	}
+	url = 'https://www.googleapis.com/youtube/v3/search';
+
+	$.getJSON(url, params, function(data){
+		showResults(data.items);
+		console.log(data.items);
+	})
+}
+
+function showResults(data){
+		var resultList = $('#search-results');
+		var list = {
+			title: '',
+			videoURL: '',
+			thumbNail: '',
+			init: function(){
+				for(var i=0; i<data.length; i++){
+					this.title = data[i].snippet.title;
+					this.videoURL = 'http://www.youtube.com/watch?v=' + data[i].id.videoId;
+					this.thumbNail= data[i].snippet.thumbnails.high.url;
+					resultList.append(this.drawList());
+				}
+			},			
+			drawList: function(){
+				return "<p><p>" + this.title + "</p><a href='" + this.videoURL + "'target='_blank'> <img src='" + this.thumbNail + "'></a></p>"
+			},
+		};
+
+		resultList.empty();		
+		list.init();
+		console.log(list);	
+	}
+
+});
